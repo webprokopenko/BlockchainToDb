a2b9fdf616369715ea289caa1e643f9d05d31991
Ivan Prokopenko <web.prokopenko@gmail.com>
2018-02-05 15:29:37 +0200

    Scan uninsert transaction block in mongoDb and insert again

--- controllers/getETHRpc.js
+++ controllers/getETHRpc.js
@@ -5,112 +5,119 @@ const math = require('mathjs');
 const RpcConnectOption = require('../config/config.json').ETHRpc
 clientRPC = new rpc.Client(RpcConnectOption);
 
-function convertNumberToHex(num){
+function convertNumberToHex(num) {
     return `0x${num.toString(16)}`
 }
-function convertHexToInt(hex){
+function convertHexToInt(hex) {
     return `${parseInt(hex, 16)}`
 }
-function getGasFromTransactionHash(hash){
-    return new Promise((resolve,reject)=>{
+function getGasFromTransactionHash(hash) {
+    return new Promise((resolve, reject) => {
         clientRPC.call(
-            {   'jsonrpc': '2.0',
-                'method':'eth_getTransactionReceipt',
-                'params':[hash],
-                'id':1
+            {
+                'jsonrpc': '2.0',
+                'method': 'eth_getTransactionReceipt',
+                'params': [hash],
+                'id': 1
             },
-            (err,res)=>{
-                if(err)
+            (err, res) => {
+                if (err)
                     return reject(err)
-                if(!res)
+                if (!res)
                     return reject(new Error(`Response body empty`));
-                if(res.error)
+                if (res.error)
                     return reject(new Error(`Error from command geth  Message: '${res.error.message}'  Code: ${res.error.code}`));
-                if(!res.result)
+                if (!res.result)
                     return reject(new Error(`Result body empty`))
-                if(!res.result.gasUsed)
+                if (!res.result.gasUsed)
                     return reject(new Error('Result gasUsed from transaction empty'))
-                
+
                 resolve(convertHexToInt(res.result.gasUsed));
             }
         )
     })
 }
-module.exports.getBlockData = getBlockData;
-    function getBlockData(numBlock){
-    return new Promise((resolve,reject)=>{
+function getBlockData(numBlock) {
+    return new Promise((resolve, reject) => {
         clientRPC.call(
-            {   'jsonrpc': '2.0',
-                'method':'eth_getBlockByNumber',
-                'params':[convertNumberToHex(numBlock), true],
-                'id':1
+            {
+                'jsonrpc': '2.0',
+                'method': 'eth_getBlockByNumber',
+                'params': [convertNumberToHex(numBlock), true],
+                'id': 1
             },
-            (err,res)=>{
-                if(err)
+            (err, res) => {
+                if (err)
                     return reject(new Error(`Error from geth: ${err}`));
-                if(!res)
+                if (!res)
                     return reject(new Error(`Response body empty`));
-                if(res.error)
+                if (res.error)
                     return reject(new Error(`Error from command geth  Message: '${res.error.message}'  Code: ${res.error.code}`));
-                if(!res.result.transactions)
+                if (!res.result.transactions)
                     return reject(new Error(`Transactions empty`))
-                
+
                 resolve(res.result);
-            }        
+            }
         )
     })
 }
-module.exports.getTransactionFromETH = async function getTransactionFromETH(numBlock){
-        try{
-            const blockData = await getBlockData(numBlock);
-            let blockTransaction = {
-                block:      parseInt(convertHexToInt(blockData.number)),
-                timestamp:  convertHexToInt(blockData.timestamp),
-                transactions:[] 
-            };
-            await Promise.all(blockData.transactions.map(async (element, i) => {
-                let transaction={};
-                transaction.from = element.from;
-                transaction.to = element.to;
-                transaction.value = Units.convert(convertHexToInt(element.value),'wei','eth');
-                transaction.fee = Units.convert((convertHexToInt(element.gas) * convertHexToInt(element.gasPrice)),'wei','eth');
-                transaction.hash = element.hash;
-                transaction.gasUse = math.bignumber(await getGasFromTransactionHash(element.hash));
-                transaction.gasPrice = math.bignumber(Units.convert(convertHexToInt(element.gasPrice), 'wei', 'eth'));
-                transaction.fee = math.multiply(transaction.gasPrice, transaction.gasUse).toFixed();
-                transaction.gasUse = transaction.gasUse.toFixed();
-                transaction.gasPrice = transaction.gasPrice.toFixed();
+async function getTransactionFromETH(numBlock) {
+    try {
+        const blockData = await getBlockData(numBlock);
+        let blockTransaction = {
+            block: parseInt(convertHexToInt(blockData.number)),
+            timestamp: convertHexToInt(blockData.timestamp),
+            transactions: []
+        };
+        await Promise.all(blockData.transactions.map(async (element, i) => {
+            let transaction = {};
+            transaction.from = element.from;
+            transaction.to = element.to;
+            transaction.value = Units.convert(convertHexToInt(element.value), 'wei', 'eth');
+            transaction.fee = Units.convert((convertHexToInt(element.gas) * convertHexToInt(element.gasPrice)), 'wei', 'eth');
+            transaction.hash = element.hash;
+            transaction.gasUse = math.bignumber(await getGasFromTransactionHash(element.hash));
+            transaction.gasPrice = math.bignumber(Units.convert(convertHexToInt(element.gasPrice), 'wei', 'eth'));
+            transaction.fee = math.multiply(transaction.gasPrice, transaction.gasUse).toFixed();
+            transaction.gasUse = transaction.gasUse.toFixed();
+            transaction.gasPrice = transaction.gasPrice.toFixed();
 
-                blockTransaction.transactions.push(transaction);
-            }));
-             return blockTransaction;
-            
-        }catch(e){
-            console.log(e);
-        }           
+            blockTransaction.transactions.push(transaction);
+        }));
+        return blockTransaction;
+
+    } catch (e) {
+        console.log(e);
+    }
 }
-module.exports.getBlockNumber = function getBlockNumber(param){
-    return new Promise((resolve,reject)=>{
-        if(!param || (param!=='latest' && param!='pending'))
+function getBlockNumber(param) {
+    return new Promise((resolve, reject) => {
+        if (!param || (param !== 'latest' && param != 'pending'))
             return reject(new Error(`function getBlockNumber missin param`));
         clientRPC.call(
-            {   'jsonrpc': '2.0',
-                'method':'eth_getBlockByNumber',
-                'params':[param, true],
-                'id':1
+            {
+                'jsonrpc': '2.0',
+                'method': 'eth_getBlockByNumber',
+                'params': [param, true],
+                'id': 1
             },
-            (err,res)=>{
-                if(err)
+            (err, res) => {
+                if (err)
                     return reject(new Error(`getBlockNumber Error from geth: ${err}`));
-                if(!res)
+                if (!res)
                     return reject(new Error(`getBlockNumber Response body empty`));
-                if(res.error)
+                if (res.error)
                     return reject(new Error(`getBlockNumber Error from command geth  Message: '${res.error.message}'  Code: ${res.error.code}`));
-                if(!res.result.number)
+                if (!res.result.number)
                     return reject(new Error(`getBlockNumber Block number empty`))
-                
+
                 resolve(parseInt(convertHexToInt(res.result.number)));
-            }        
+            }
         )
     })
+}
+module.exports = {
+    getBlockNumber:         getBlockNumber,
+    getBlockData:           getBlockData,
+    getTransactionFromETH:  getTransactionFromETH
 }
--- exec/ETHSaveTransactionToMongo.js
+++ exec/ETHSaveTransactionToMongo.js
@@ -18,58 +18,88 @@ mongoose.connect(mongodbConnectionString);
 //Arguments listener
 const argv = require('minimist')(process.argv.slice(2));
 
+const fs = require('fs');
+const path = require('path');
 
-function saveBlockTransactionToMongoDb(blockData){
-    return new Promise((resolve,reject)=>{
-            try {
-                blockData = new BlockTransaction(blockData);
-                blockData.save()
-                    .then(item => {
-                        resolve(true);
-                    })
-                    .catch(e => {
-                        reject(e);
-                    });
-            } catch (error) {
-                reject(error)
-            }
-        })
+function saveBlockTransactionToMongoDb(blockData) {
+    return new Promise((resolve, reject) => {
+        try {
+            blockData = new BlockTransaction(blockData);
+            blockData.save()
+                .then(item => {
+                    resolve(true);
+                })
+                .catch(e => {
+                    reject(e);
+                });
+        } catch (error) {
+            reject(error)
+        }
+    })
 }
-function getLastBlockTransactionMongoDb(){
-    return new Promise((resolve,reject)=>{
-        try{
+function getLastBlockTransactionMongoDb() {
+    return new Promise((resolve, reject) => {
+        try {
             BlockTransaction
                 .findOne({})
-                .sort({block:-1})
-                .then(res=>{
+                .sort({ block: -1 })
+                .then(res => {
                     res.block ? resolve(res.block) : reject(new Error(`function getLastBlockTransactionMongoDb no block number`));
                 })
-                .catch(e=>reject(e));
-        } catch(error){
+                .catch(e => reject(e));
+        } catch (error) {
             reject(error);
         }
     });
 }
-async function saveBlockTransactionLastinMongoDb(order){
-    if(!order) return LoggerTransactionToDbError.error('function saveBlockTransactionLastinMongoDb order is empty');
+function getFirstBlockTransactionMongoDb() {
+    return new Promise((resolve, reject) => {
+        try {
+            BlockTransaction
+                .findOne({})
+                .then(res => {
+                    res.block ? resolve(res.block) : reject(new Error(`function getFirstBlockTransactionMongoDb no block number`));
+                })
+                .catch(e => reject(e));
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+async function existBlock(numBlock){
+    return new Promise((resolve, reject) => {
+        try {
+            BlockTransaction
+                .findOne({block:numBlock})
+                .then(res => {
+                    res ? resolve(true) : resolve(false);
+                })
+                .catch(e => reject(e));
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+async function saveBlockTransactionLastinMongoDb(order) {
+    if (!order) return LoggerTransactionToDbError.error('function saveBlockTransactionLastinMongoDb order is empty');
     try {
         let lastBlockInMongoDb = await getLastBlockTransactionMongoDb();
         let lastBlockInEthereum = await getETHRpc.getBlockNumber('latest');
-        saveBlockTransactionFromTo(lastBlockInMongoDb+1, lastBlockInEthereum, order);
+        saveBlockTransactionFromTo(lastBlockInMongoDb + 1, lastBlockInEthereum, order);
     } catch (error) {
         LoggerTransactionToDbError.error('function saveBlockTransactionLastinMongoDb ' + error);
     }
-    
+
 }
-async function saveBlockTransactionFromTo(from, to, order){
+async function saveBlockTransactionFromTo(from, to, order) {
     const taskQue = new Quequ(order);
     for (let i = from; i <= to; i++) {
-        taskQue.pushTask(async done=>{  
+        taskQue.pushTask(async done => {
             try {
                 blockData = await getETHRpc.getTransactionFromETH(i);
                 await saveBlockTransactionToMongoDb(blockData);
                 console.log(` Write OK! ${i}`);
-                done();    
+                done();
             } catch (error) {
                 LoggerTransactionToDbBadBlock.error(i);
                 LoggerTransactionToDbError.error(error);
@@ -78,22 +108,79 @@ async function saveBlockTransactionFromTo(from, to, order){
         })
     }
 }
-if(argv){
-    if(argv.from && argv.to && argv.order){
-        saveBlockTransactionFromTo(argv.from,argv.to, argv.order);
+async function scanBadBlockfromMongoDbAndSave(){
+    const taskQue = new Quequ(5);
+    try {
+        let from    = await getFirstBlockTransactionMongoDb();
+        let to      = await getLastBlockTransactionMongoDb();
+        for (let i = from; i <= to; i++) {
+            let exists =  await existBlock(i);
+            if(!exists){
+                taskQue.pushTask(async done => {
+                    try {
+                        blockData = await getETHRpc.getTransactionFromETH(i);
+                        await saveBlockTransactionToMongoDb(blockData);
+                        console.log(` Write OK! ${i}`);
+                        done();
+                    } catch (error) {
+                        LoggerTransactionToDbBadBlock.error(i);
+                        LoggerTransactionToDbError.error(error);
+                        done();
+                    }
+                })
+            }
+        }
+    } catch (error) {
+        console.log('Error ' + error);
     }
-    if(argv.latest && argv.order){
+}
+function scanBadBlock(filename, callback) {
+    fs.readFile(path.normalize(filename), 'utf8', (err, data) => {
+        if (err)
+            return callback(err, null)
+        let tmp = data.split('\n');
+        return callback(null, tmp)
+    });
+}
+function saveBadBlock() {
+    scanBadBlock('../logs/transactionsToDb/badblock.log', (err, data) => {
+        if (err) console.log('Error scan block' + err);
+        const taskQue = new Quequ(10);
+        data.forEach(element => {
+            taskQue.pushTask(async done => {
+                try {
+                    blockData = await getETHRpc.getTransactionFromETH(element);
+                    await saveBlockTransactionToMongoDb(blockData);
+                    console.log(` Write OK! ${element}`);
+                    done();
+                } catch (error) {
+                    LoggerTransactionToDbBadBlock.error(element);
+                    LoggerTransactionToDbError.error(error);
+                    done();
+                }
+            })
+        });
+    });
+}
+if (argv) {
+    if (argv.from && argv.to && argv.order) {
+        saveBlockTransactionFromTo(argv.from, argv.to, argv.order);
+    }
+    if (argv.latest && argv.order) {
         saveBlockTransactionLastinMongoDb(argv.order);
     }
-    if(argv.getlastblock){
-        getETHRpc.getBlockNumber('latest').then(block=>{
+    if (argv.getlastblock) {
+        getETHRpc.getBlockNumber('latest').then(block => {
             console.log(block);
         })
     }
-    if(argv.getblock && argv.getblock>0){
-        getETHRpc.getBlockData(argv.getblock).then(block=>{
+    if (argv.getblock && argv.getblock > 0) {
+        getETHRpc.getBlockData(argv.getblock).then(block => {
             console.log(block);
         })
+    }
+    if (argv.savebadblock){
+        scanBadBlockfromMongoDbAndSave();
     }
 }
 
