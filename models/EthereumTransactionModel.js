363580589d3c5d700e9e0c854d455b32a60fc2f1
Ivan Prokopenko <web.prokopenko@gmail.com>
2018-02-07 11:03:58 +0200

    Add new Transaction Model

---
+++ models/EthereumTransactionModel.js
@@ -0,0 +1,16 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let BlockTransaction = new Schema({
+    hash: {
+        type: String,
+        index: { unique: true }
+    },
+    timestamp:  String,
+    from:       String,
+    to:         String,
+    value:      String,
+    fee:        String,
+});
+
+mongoose.model('ethereumTransactions', BlockTransaction);
