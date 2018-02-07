f5e48591462b6f6281f84e4244e5553975f8d0e6
Ivan Prokopenko <web.prokopenko@gmail.com>
2018-02-07 12:06:10 +0200

    ETHSaveTransaction for new model finish

--- exec/ETHSaveTransactionToMongo.js
+++ exec/ETHSaveTransactionToMongo.js
@@ -1,5 +1,5 @@
 
-require('../models/BlockTransactionModel.js');
+require('../models/EthereumTransactionModel.js');
 const getETHRpc = require('../lib/ethereum/getETHRpc');
 const Quequ = require('../lib/TaskQueue');
 const mongodbConnectionString = require('../config/config.json').mongodbConnectionString;
@@ -12,7 +12,7 @@ LoggerTransactionToDbBadBlock.setLevel(LoggerTransactionToDbBadBlock.INFO).addHa
 LoggerTransactionToDbError.setLevel(LoggerTransactionToDbError.ERROR).addHandler(new intel.handlers.File('../logs/transactionsToDb/eror.log'));
 //Mongoose
 const mongoose = require('mongoose');
-const BlockTransaction = mongoose.model('blockTransaction');
+const BlockTransaction = mongoose.model('ethereumTransactions');
 mongoose.connect(mongodbConnectionString);
 
 //Arguments listener
@@ -37,67 +37,17 @@ function saveBlockTransactionToMongoDb(blockData) {
         }
     })
 }
-function getLastBlockTransactionMongoDb() {
-    return new Promise((resolve, reject) => {
-        try {
-            BlockTransaction
-                .findOne({})
-                .sort({ block: -1 })
-                .then(res => {
-                    res.block ? resolve(res.block) : reject(new Error(`function getLastBlockTransactionMongoDb no block number`));
-                })
-                .catch(e => reject(e));
-        } catch (error) {
-            reject(error);
-        }
-    });
-}
-function getFirstBlockTransactionMongoDb() {
-    return new Promise((resolve, reject) => {
-        try {
-            BlockTransaction
-                .findOne({})
-                .then(res => {
-                    res.block ? resolve(res.block) : reject(new Error(`function getFirstBlockTransactionMongoDb no block number`));
-                })
-                .catch(e => reject(e));
-        } catch (error) {
-            reject(error);
-        }
-    });
-}
-async function existBlock(numBlock){
-    return new Promise((resolve, reject) => {
-        try {
-            BlockTransaction
-                .findOne({block:numBlock})
-                .then(res => {
-                    res ? resolve(true) : resolve(false);
-                })
-                .catch(e => reject(e));
-        } catch (error) {
-            reject(error);
-        }
-    });
-}
-async function saveBlockTransactionLastinMongoDb(order) {
-    if (!order) return LoggerTransactionToDbError.error('function saveBlockTransactionLastinMongoDb order is empty');
-    try {
-        let lastBlockInMongoDb = await getLastBlockTransactionMongoDb();
-        let lastBlockInEthereum = await getETHRpc.getBlockNumber('latest');
-        saveBlockTransactionFromTo(lastBlockInMongoDb + 1, lastBlockInEthereum, order);
-    } catch (error) {
-        LoggerTransactionToDbError.error('function saveBlockTransactionLastinMongoDb ' + error);
-    }
-
-}
 async function saveBlockTransactionFromTo(from, to, order) {
     const taskQue = new Quequ(order);
     for (let i = from; i <= to; i++) {
         taskQue.pushTask(async done => {
             try {
                 blockData = await getETHRpc.getTransactionFromETH(i);
-                await saveBlockTransactionToMongoDb(blockData);
+                if (blockData) {
+                    await Promise.all(blockData.map(async (element, i) => {
+                        await saveBlockTransactionToMongoDb(element)
+                    }));
+                }
                 console.log(` Write OK! ${i}`);
                 done();
             } catch (error) {
@@ -108,68 +58,9 @@ async function saveBlockTransactionFromTo(from, to, order) {
         })
     }
 }
-async function scanBadBlockfromMongoDbAndSave(){
-    const taskQue = new Quequ(5);
-    try {
-        let from    = await getFirstBlockTransactionMongoDb();
-        let to      = await getLastBlockTransactionMongoDb();
-        for (let i = from; i <= to; i++) {
-            let exists =  await existBlock(i);
-            if(!exists){
-                taskQue.pushTask(async done => {
-                    try {
-                        blockData = await getETHRpc.getTransactionFromETH(i);
-                        await saveBlockTransactionToMongoDb(blockData);
-                        console.log(` Write OK! ${i}`);
-                        done();
-                    } catch (error) {
-                        LoggerTransactionToDbBadBlock.error(i);
-                        LoggerTransactionToDbError.error(error);
-                        done();
-                    }
-                })
-            }else{
-                console.log(` Block exist ${i}`)
-            }
-        }
-    } catch (error) {
-        console.log('Error ' + error);
-    }
-}
-function scanBadBlock(filename, callback) {
-    fs.readFile(path.normalize(filename), 'utf8', (err, data) => {
-        if (err)
-            return callback(err, null)
-        let tmp = data.split('\n');
-        return callback(null, tmp)
-    });
-}
-function saveBadBlock() {
-    scanBadBlock('../logs/transactionsToDb/badblock.log', (err, data) => {
-        if (err) console.log('Error scan block' + err);
-        const taskQue = new Quequ(10);
-        data.forEach(element => {
-            taskQue.pushTask(async done => {
-                try {
-                    blockData = await getETHRpc.getTransactionFromETH(element);
-                    await saveBlockTransactionToMongoDb(blockData);
-                    console.log(` Write OK! ${element}`);
-                    done();
-                } catch (error) {
-                    LoggerTransactionToDbBadBlock.error(element);
-                    LoggerTransactionToDbError.error(error);
-                    done();
-                }
-            })
-        });
-    });
-}
 if (argv) {
     if (argv.from && argv.to && argv.order) {
         saveBlockTransactionFromTo(argv.from, argv.to, argv.order);
-    }
-    if (argv.latest && argv.order) {
-        saveBlockTransactionLastinMongoDb(argv.order);
     }
     if (argv.getlastblock) {
         getETHRpc.getBlockNumber('latest').then(block => {
@@ -181,8 +72,8 @@ if (argv) {
             console.log(block);
         })
     }
-    if (argv.savebadblock){
-        scanBadBlockfromMongoDbAndSave();
+    if (argv.saveblock && argv.saveblock > 0) {
+           
     }
 }
 
