2c337cda77203753ecd21cb9d13f313058762aa9
Ivan Prokopenko <web.prokopenko@gmail.com>
2018-02-16 14:34:05 +0200

    Check BitfinexTo Mongo

--- exec/ExchangeBitfinexToMongo.js
+++ exec/ExchangeBitfinexToMongo.js
@@ -8,23 +8,27 @@ const request = require('request');
 const Quequ = require('../lib/TaskQueue');
 //dbEthertransactionsLib
 const dbExchangeLib = require('../lib/mongodb/exchange.js');
-
+//Arguments listener
+const argv = require('minimist')(process.argv.slice(2));
 
 let url = 'https://api.bitfinex.com/v2';
-async function getBitnifexCurrency(urlApi, pair1, pair2, day){
+
+async function getBitnifexCurrency(urlApi, pair1, pair2, from, to){
     const taskQue = new Quequ(5);
-    let from = new Date(day - 60*60*120*1000);
     request.get(
         `${urlApi}/candles/trade:1h:t${pair1}${pair2}/hist?start=`
         + `${from.getTime()}` + `&end=`
-        + `${day.getTime()}`,
+        + `${to.getTime()}`,
 
         async (error, response, body) => {
             if(!body || body.length<2){
                 console.log('Error: Body empty' + body);    
             }
+            if(error){
+                console.log('Error: request get ' + error);    
+            }
             body  = JSON.parse(body) || {};
-            if(body.length===120){
+            
                 console.log(body.length);
                 await Promise.all(body.map(async (element) => {
                     taskQue.pushTask(async done => {
@@ -39,34 +43,27 @@ async function getBitnifexCurrency(urlApi, pair1, pair2, day){
             
                         dbExchangeLib.saveExchangeToMongoDb(rateData)
                         .then(save => {
-                            console.log(save);
                             done();
                         })
                         .catch(error=>{
                             if(error.code!=11000)
-                                console.error('Error saveHistoricRates' + error);
+                                console.error('Error getBitnifexCurrency' + error);
                             done();
                         })
                     })
                 }))
-                
                 return false;        
-            }
         }
     )
 }
-async function saveBtcUsc(){
-    try{
-        let i=24;
-        let j=i*2
-        while(i <= j){
-                let day = new Date(new Date() - 60*60*i*1000)
-                let currencyBitfinex = await getBitnifexCurrency(url,'BTC','USD', day);
-                i = i+24;    
-        }
-    }catch(e){
-        console.log('Error: ' + e)
-    }
+
+if (argv.savebitnifex && argv.from && argv.to) {
+    console.log('Bitnifex Scan and save from to Started ..... ');
+    getBitnifexCurrency(url,'BTC','USD', new Date(argv.from), new Date(argv.to));
+    getBitnifexCurrency(url,'BTC','EUR', new Date(argv.from), new Date(argv.to));
+    getBitnifexCurrency(url,'ETH','USD', new Date(argv.from), new Date(argv.to));
+    getBitnifexCurrency(url,'ETH','EUR', new Date(argv.from), new Date(argv.to));
 }
-saveBtcUsc();
+
+
 
