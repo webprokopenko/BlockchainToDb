af7a9abccfaf40fdc3429faa5093051f8c3b3f56
Ivan Prokopenko <web.prokopenko@gmail.com>
2018-02-16 18:18:50 +0200

    Add Api stats bitfinex

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -5,7 +5,7 @@ const StatsError = intel.getLogger('StatsError');
 StatsError.setLevel(StatsError.ERROR).addHandler(new intel.handlers.File(`${appRoot}/logs/stats/eror.log`));
 
 /**
- * Return exchange from mongodb from now DateTime to - countMonths
+ * Return exchange Gdax from mongodb from now DateTime to - countMonths
  * @param {string} pair pairs for exchange like BTC-USD
  * @param {number} countMonths count months from 1 to 12
  * @return {(object|Error)} list pairs
@@ -25,6 +25,28 @@ async function getGdax(pair, countMonths){
         StatsError.error(`Error: getGdax: ${error}`)
     }
 }
+/**
+ * Return exchange Bitfinex from mongodb from now DateTime to - countMonths
+ * @param {string} pair pairs for exchange like BTC-USD
+ * @param {number} countMonths count months from 1 to 12
+ * @return {(object|Error)} list pairs
+ */
+async function getBitfinex(pair, countMonths){
+    if(!countMonths || countMonths<1 || countMonths>12)
+         throw new Error('Months can be from 1 to 12'); 
+    try {
+        let from = new Date();
+        let to = new Date();
+        to = to.setMonth(to.getMonth() - countMonths);
+        from = Math.floor(from/1000);
+        to = Math.floor(to/1000);
+        let list = await exchange.getExchangeList(2, pair, from, to);
+        return list;
+    } catch (error) {
+        StatsError.error(`Error: getBitfinex: ${error}`)
+    }
+}
 module.exports = {
-    getGdax:        getGdax
+    getGdax:        getGdax,
+    getBitfinex:    getBitfinex
 }
--- routes/api/v4.0/stats/index.js
+++ routes/api/v4.0/stats/index.js
@@ -42,4 +42,44 @@ app.get('/ETH-EUR/GDAX/:countMonths', (req, res, next) => {
             next(error);
         })
 });
+app.get('/BTC-USD/BITFINEX/:countMonths', (req, res, next) => {
+    const countMonths = req.params.countMonths;
+    statsController.getBitfinex('BTC-USD', countMonths)
+        .then(list => {
+            res.send(list);
+        })
+        .catch(error => {
+            next(error)
+        })
+});
+app.get('/BTC-EUR/BITFINEX/:countMonths', (req, res, next) => {
+    const countMonths = req.params.countMonths;
+    statsController.getBitfinex('BTC-EUR', countMonths)
+        .then(list => {
+            res.send(list);
+        })
+        .catch(error => {
+            next(error)
+        })
+});
+app.get('/ETH-USD/BITFINEX/:countMonths', (req, res, next) => {
+    const countMonths = req.params.countMonths;
+    statsController.getBitfinex('ETH-USD',countMonths)
+        .then(list => {
+            res.send(list);
+        })
+        .catch(error => {
+            next(error)
+        })
+});
+app.get('/ETH-EUR/BITFINEX/:countMonths', (req, res, next) => {
+    const countMonths = req.params.countMonths;
+    statsController.getBitfinex('ETH-EUR',countMonths)
+        .then(list => {
+            res.send(list);
+        })
+        .catch(error => {
+            next(error);
+        })
+});
 
