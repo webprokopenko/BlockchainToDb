5ac767ae68c3ccacfea68218a1e833836183dc5d
Ivan Prokopenko <web.prokopenko@gmail.com>
2018-03-26 10:57:14 +0300

    Add ListUnspent, gettxout

--- controllers/btcController.js
+++ controllers/btcController.js
@@ -13,6 +13,31 @@ async function getBalance(address){
         BtcError.error(`${new Date()} Error: getBalance: ${error}`);
     }
 }
+async function sendRawTransaction(raw){
+    try {
+        let res = await gethBTC.sendRawTransacitonCmd(raw);
+        return {'res':res};
+    } catch (error) {
+        BtcError.error(`${new Date()} Error: sendRawTransaction: ${error}`);
+    }
+}
+async function listUnspent(addr){
+    try{
+        return await gethBTC.listUnspentCmd(addr);
+    }catch (error){
+        BtcError.error(`${new Date()} Error: listUnspen: ${error}`);
+    }
+}
+async function gettxout(txid){
+    try{
+        return await gethBTC.listUnspentCmd(txid);
+    }catch (error){
+        BtcError.error(`${new Date()} Error: gettxout: ${error}`);
+    }
+}
 module.exports = {
     getBalance:             getBalance,
+    sendRawTransaction:     sendRawTransaction,
+    listUnspent:            listUnspent,
+    gettxout:               gettxout,
 }
