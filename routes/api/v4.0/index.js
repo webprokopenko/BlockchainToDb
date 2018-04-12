e476c301ef75af5bf583d857025b25401b8a4468
Mykola <mykola_borodyn@ecoengineer.in.ua>
2018-04-12 11:09:21 +0300

    routes bch

--- routes/api/v4.0/index.js
+++ routes/api/v4.0/index.js
@@ -4,7 +4,7 @@ const app = module.exports = express();
 app.use('/ETH', require('./eth'));
 app.use('/BTC', require('./btc'));
 app.use('/LTC', require('./ltc'));
-app.use('/BTC', require('./btc'));
+app.use('/BCH', require('./bch'));
 app.use('/BTG', require('./btg'));
 app.use('/stats', require('./stats'));
 app.get('/', (req, res) => {
