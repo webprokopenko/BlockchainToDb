8cdb294ef7085e670921e5e0eed5b17f42b95b8e
Prokopenko <prokopenko.cnw@gmail.com>
2018-05-14 12:07:02 +0300

    Add cluster script

---
+++ clusteredApp.js
@@ -0,0 +1,12 @@
+const cluster = require('cluster');
+const os = require('os');
+
+if(cluster.isMaster){
+    const cpus = os.cpus.length;
+    console.log(`Clustering to ${cpus} CPUs`);
+    for(let i = 0; i<cpus; i++){
+        cluster.fork();
+    }
+}else{
+    require('./app');;
+}
