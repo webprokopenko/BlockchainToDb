2a180a0ef176b1d935edd5226c4a4fcec05cd39e
Prokopenko <prokopenko.cnw@gmail.com>
2018-05-14 12:37:01 +0300

    Check cluster, --save package needs

--- clusteredApp.js
+++ clusteredApp.js
@@ -2,7 +2,7 @@ const cluster = require('cluster');
 const os = require('os');
 
 if(cluster.isMaster){
-    const cpus = os.cpus.length;
+    const cpus = os.cpus().length;
     console.log(`Clustering to ${cpus} CPUs`);
     for(let i = 0; i<cpus; i++){
         cluster.fork();
