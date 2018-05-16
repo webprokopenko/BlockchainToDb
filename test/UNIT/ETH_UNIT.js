4750e1c5111fb4061f6cbfb7ca82bebd6859fc38
Prokopenko <prokopenko.cnw@gmail.com>
2018-05-16 19:01:48 +0300

    Add ETH UNIT test

--- test/UNIT/ETH_UNIT.js
+++ test/UNIT/ETH_UNIT.js
@@ -7,39 +7,51 @@ const utils = require(`${appRoot}/lib/ethereum/utilsETH`);
 
 const TEST_DATA = require('../test-data.json');
 
-describe('ETHRpc',  async ()=> {
-  it('getBlockData() should return data in block',  async ()=> {
-    let BlockData =  await ETHRpc.getBlockData(TEST_DATA.block.num);
-    expect(BlockData).to.include.all.keys('difficulty','extraData','gasLimit','gasUsed','hash','logsBloom',
-        'miner','mixHash','nonce','number','parentHash','receiptsRoot','sha3Uncles','size','stateRoot','timestamp',
-        'totalDifficulty','transactions','transactionsRoot','uncles');
-  });
-  it('getGasPrice() should return gas price in Ethereum',  async ()=> {
-    let gasPrice =  await ETHRpc.getGasPrice();
-    expect(utils.convertHexToInt(gasPrice)).to.be.an('string');
-    expect(parseFloat(utils.convertHexToInt(gasPrice))).to.be.above(0);
-  });
-  it('getLatestBlock() should return last block in Ethereum',  async ()=> {
-    let lastBlock =  await ETHRpc.getLatestBlock();
-    expect(lastBlock).to.include.all.keys('difficulty','extraData','gasLimit','gasUsed','hash','logsBloom',
-        'miner','mixHash','nonce','number','parentHash','receiptsRoot','sha3Uncles','size','stateRoot','timestamp',
-        'totalDifficulty','transactions','transactionsRoot','uncles');
-  });
-  it('getBalance() should return balance of account',  async ()=> {
-    let balance =  await ETHRpc.getBalance(TEST_DATA.wallet.addres);
-    expect(utils.convertHexToInt(balance)).to.be.equal(TEST_DATA.wallet.balance);
-  });
-  it('getTransactionCount() should return count transaction of account',  async ()=> {
-    let count =  await ETHRpc.getTransactionCount(TEST_DATA.wallet.addres);
-    expect(utils.convertHexToInt(count)).to.be.equal(TEST_DATA.wallet.transactionCount);
-  });
-  it('getTransactionFromHash() should return transaction from Hash',  async ()=> {
-    let count =  await ETHRpc.getTransactionFromHash(TEST_DATA.wallet.TxHash);
-    expect(count).include.keys('blockHash','blockNumber','from','gas','gasPrice','hash','input','nonce','to','transactionIndex','value','v','r','s');
-  });
-  it('getTransactionCountETH() should return count of transaction in block',  async ()=> {
-    let count =  await ETHRpc.getTransactionCountETH(1244073);
-    expect(count).equal(5);
-  });
-  
+describe('ETHRpc', async () => {
+
+    it('getGasFromTransactionHash() should return gas from Transaction Hash', async () => {
+        let gas = await ETHRpc.getGasFromTransactionHash(TEST_DATA.wallet.TxHash);
+        expect(gas).to.be.equal(TEST_DATA.wallet.TxHashGas);
+    });
+
+    it('getBlockData() should return data in block', async () => {
+        let BlockData = await ETHRpc.getBlockData(TEST_DATA.block.num);
+        expect(BlockData).to.include.all.keys('difficulty', 'extraData', 'gasLimit', 'gasUsed', 'hash', 'logsBloom',
+            'miner', 'mixHash', 'nonce', 'number', 'parentHash', 'receiptsRoot', 'sha3Uncles', 'size', 'stateRoot', 'timestamp',
+            'totalDifficulty', 'transactions', 'transactionsRoot', 'uncles');
+    });
+    it('getGasPrice() should return gas price in Ethereum', async () => {
+        let gasPrice = await ETHRpc.getGasPrice();
+        expect(utils.convertHexToInt(gasPrice)).to.be.an('string');
+        expect(parseFloat(utils.convertHexToInt(gasPrice))).to.be.above(0);
+    });
+    it('getLatestBlock() should return last block in Ethereum', async () => {
+        let lastBlock = await ETHRpc.getLatestBlock();
+        expect(lastBlock).to.include.all.keys('difficulty', 'extraData', 'gasLimit', 'gasUsed', 'hash', 'logsBloom',
+            'miner', 'mixHash', 'nonce', 'number', 'parentHash', 'receiptsRoot', 'sha3Uncles', 'size', 'stateRoot', 'timestamp',
+            'totalDifficulty', 'transactions', 'transactionsRoot', 'uncles');
+    });
+    it('getBalance() should return balance of account', async () => {
+        let balance = await ETHRpc.getBalance(TEST_DATA.wallet.addres);
+        expect(utils.convertHexToInt(balance)).to.be.equal(TEST_DATA.wallet.balance);
+    });
+    it('getTransactionCount() should return count transaction of account', async () => {
+        let count = await ETHRpc.getTransactionCount(TEST_DATA.wallet.addres);
+        expect(utils.convertHexToInt(count)).to.be.equal(TEST_DATA.wallet.transactionCount);
+    });
+    it('getTransactionFromHash() should return transaction from Hash', async () => {
+        let count = await ETHRpc.getTransactionFromHash(TEST_DATA.wallet.TxHash);
+        expect(count).include.keys('blockHash', 'blockNumber', 'from', 'gas', 'gasPrice', 'hash', 'input', 'nonce', 'to', 'transactionIndex', 'value', 'v', 'r', 's');
+    });
+    it('getTransactionCountETH() should return count of transaction in block', async () => {
+        let count = await ETHRpc.getTransactionCountETH(TEST_DATA.block.num);
+        expect(count).equal(5);
+    });
+    it('getTransactionFromETH() should return transactions in block', async () => {
+        let transactions = await ETHRpc.getTransactionFromETH(TEST_DATA.block.num);
+        let EmptyTransactions = await ETHRpc.getTransactionFromETH(TEST_DATA.block.emptyTransaction);
+        expect(transactions).include.keys('0', '1', '2', '3', '4');
+        expect(EmptyTransactions).to.be.equal(null);
+    });
+
 });
