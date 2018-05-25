0f65c3b81f74daae5ca3f14529d296a89d3714cb
Mykola <mykola_borodyn@ecoengineer.in.ua>
2018-05-25 15:13:23 +0300

    TEST GETH RPC SERVICE

---
+++ test/SERVICES/geth_methods.js
@@ -0,0 +1,43 @@
+const testData = require(appRoot + '/test/SERVICES/geth_config.json');
+const utils = require(appRoot + '/lib/ethereum/utilsETH');
+
+module.exports = {
+    eth_getBalance: (params) => {
+        if(!utils.isArray(params) || !utils.isAddress(params[0]) || !params[1])
+        {
+            throw Error('Wrong input params');
+        } else {
+            const str = ['latest', 'earliest', 'pending'];
+            if(str.indexOf(params[1]) >= 0) {
+                return {balance: testData.balance};
+            } else throw Error('Invalid argument 1');
+        }
+    },
+    eth_getBlockByNumber: (params) => {
+        if(!utils.isArray(params) || !params[0])
+        {
+            throw Error('Wrong input params');
+        } else return testData.block;
+    },
+    eth_gasPrice: () => {
+        return testData.gasPrice;
+    },
+    eth_getTransactionReceipt: (params) => {
+        if(!utils.isArray(params) || !utils.isAddress(params[0]))
+        {
+            throw Error('Wrong input params');
+        } else return testData.transactionReceipt;
+    },
+    eth_getTransactionCount: () => {
+        return testData.transactionCount;
+    },
+    eth_sendRawTransaction: () => {
+        return testData.hash;
+    },
+    eth_getTransactionByHash: (params) => {
+        if(!utils.isArray(params) || !params[0])
+        {
+            throw Error('Wrong input params');
+        } else return testData.transaction;
+    }
+};
---
+++ test/SERVICES/geth_rpc.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+//set global AppDirectory
+global.appRoot = path.resolve(__dirname + '/../../');
+
+const rpcConfig = require(appRoot + '/config/config.json').ETHRpc;
+// body parser set
+app.use(bodyParser.json({}));
+
+// rpc routes
+require(appRoot + '/test/SERVICES/geth_router')(app);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+// error handler
+app.use(function(err, req, res, next) {
+    //render the error page
+    res.status(err.status || 500);
+    res.send({error: err.message, code: err.codeErr});
+});
+
+server.listen(rpcConfig.port, function() {
+    console.log('TEST GETH RPC SERVICE start on: ' + server.address().port);
+});
+
+module.exports = app;
