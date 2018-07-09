b84c0c49ddad2482f4a6db86f89c90ab62b7b97f
Mykola <mykola_borodyn@ecoengineer.in.ua>
2018-07-09 23:46:38 +0300

    factory BaseController

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,9 @@ const controllers = [];
 config.routes.api.forEach(api => {
     config.currencies.forEach(curr => {
         const controller = controllerFactory.getController(curr, api);
-        if (!controller) {
-            console.log('Controller Code: ' + curr.code + ' API: ' + api + ' start error');
+        if (controller.error) {
+            console.log('Controller Code: ' + curr.code + ' API: ' + api + ' start error ' +
+            controller.error.message);
         } else controllers.push(controller);
     });
 });
@@ -18,22 +19,33 @@ function originalUriParser(uri) {
         return false;
     }
 }
-function selectRoute(req, res) {
+function routeRun(req, res, next) {
     const route = originalUriParser(req.originalUrl);
-    if (!route || route.length < 2 || !config.routes.indexOf(route[1] >= 0)) {
-        res.status(404).send('Wrong request route');
-    } else if (route[1] !== 'api' || route.length < 3) {
+    if (
+        !route
+        || route.length < 3
+        || route[1] !== 'api'
+    ) {
         res.status(404).send('Wrong request route');
     } else {
-        const selectedControllers = controllers.filter(controller => (
+        const controller = controllers.filter(controller => (
             controller.code === route[3] && controller.api === route[4]
-        ));
-        if (selectedControllers.length === 0) {
+        ))[0];
+        if (!controller) {
             res.status(404).send('Wrong request api');
-        } else selectedControllers[0](req, res);
-    }
+        } else {
+            req.params = route.slice(5);
+            controller(req)
+                .then(result => {
+                    res.send(result);
+                })
+                .catch(error => {
+                    next(error);
+                });
+            }
+        }
 }
 module.exports = (app) => {
-  app.get('/', selectRoute);
+  app.get('/', routeRun);
   // app.post('/', selectRoute);
 };
